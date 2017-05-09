a4101e5d660c2ecae574b9edaf1f05e2f280007c
Sherilyn Moline <smoline@gmail.com>
2017-05-09 10:48:39 -0400

    Favorites model, controller, helper, and js structure complete, only need index and show if statements added and custom settings for the index-overrides.scss files added

--- bidtastic/config/routes.rb
+++ bidtastic/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'favorites/create'
+
+  get 'favorites/destroy'
+
   resources :items
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -12,5 +16,8 @@ Rails.application.routes.draw do
   get '/login' => 'session#new'
   post '/login' => 'session#create'
   get '/logout' => 'session#destroy'
+
+  post 'favorites/:itemId'           => 'favorites#create'
+  delete 'favorites/destroy/:itemId' => 'favorites#destroy'
 
 end

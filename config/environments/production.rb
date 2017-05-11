50b13b34fe99d48850551e3f8f469f2e0752fd97
Jose A. Campos <jjoseccampos@gmail.com>
2017-05-11 16:07:13 -0400

    Updates gmail config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "dotrb-dibtastic.herokuapp.com",
+    domain: "gmail.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
